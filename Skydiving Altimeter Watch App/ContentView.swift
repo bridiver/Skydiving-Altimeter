485a72f00397ad75ae0bb1e904e18f9c6fd2f68b
bridiver <34129+bridiver@users.noreply.github.com>
2024-04-01 20:45:29 -0700

    initial commit

--- Skydiving Altimeter Watch App/ContentView.swift
+++ Skydiving Altimeter Watch App/ContentView.swift
@@ -6,14 +6,45 @@
 //
 
 import SwiftUI
+import CoreMotion
+
+class Altitude: ObservableObject {
+    @Published var value = 0 {
+        didSet {
+            let numberFormatter = NumberFormatter()
+            numberFormatter.numberStyle = .decimal
+            textValue = numberFormatter.string(from: NSNumber(value:value))!
+        }
+    }
+    @Published var textValue = "0"
+    @Published var altimeter = CMAltimeter()
+    @Published var motionManager = CMMotionActivityManager()
+    init() {
+        self.motionManager.queryActivityStarting(from: .now, to: .now, to: .main) { _, _
+            in
+            if CMAltimeter.isAbsoluteAltitudeAvailable() {
+                self.value = 10
+                self.altimeter.startAbsoluteAltitudeUpdates(to: OperationQueue.main) { (data, error) in
+                    self.value = Int(data?.altitude ?? 0)
+                }
+            } else {
+                self.value = 100
+            }}
+    }
+    deinit {
+        if CMAltimeter.isAbsoluteAltitudeAvailable() {
+            self.altimeter.stopAbsoluteAltitudeUpdates()
+        }
+    }
+}
 
 struct ContentView: View {
+    @StateObject var altitude = Altitude()
+        
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(altitude.textValue) ft")
+                        .font(.largeTitle)
         }
         .padding()
     }
